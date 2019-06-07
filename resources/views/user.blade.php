a144920004411bcbfda87a74095edc86579c65b9
Thijs Nijhof <tnijhof91@gmail.com>
2019-06-07 12:27:22 +0100

    play around with routes

---
+++ resources/views/user.blade.php
@@ -0,0 +1,1 @@
+Hello Im the user page!Wooo!
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,34 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return "Hello guys!";
+// })->name('home');
+
+// Route::get('/contact', 'ContactController@index');
+
+// Route::view('/contact', 'contact', ['name'=> 'Francis'])->name('contact');
+// Route::redirect('/', '/contact');
+
+// Route::get('/user/{name}/{id}', function($name,$id){
+//     return 'i am '.$name.' and my id is '.$id;
+// });
+
+// Route::resources([
+//     'contact' => 'ContactController', 
+//     'user' => 'UserController'
+// ]);
+
+// Route::resource('contact', 'ContactController');
+
+// Route::resource('contact', 'ContactController')->only([
+//     'index', 'show'
+// ]);
+
+Route::get('/contact/{user}/{age}/{name}', 'ContactController@index');
+
+Route::resource('contact', 'ContactController')->except([
+    'create','store', 'update','destroy'
+])->names([
+    'edit' => 'contact.superawesome'
+]);
