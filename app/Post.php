a1ede8b95782e154b952ccbbbd41c7f04c545cd7
Thijs Nijhof <tnijhof91@gmail.com>
2019-06-12 20:21:13 +0100

    crud operations with eloquent

--- app/Post.php
+++ app/Post.php
@@ -3,8 +3,16 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
 {
-    //
+    use SoftDeletes;
+    protected $dates = ['deleted_at'];
+    
+    protected $fillable = [
+        'title',
+        'body',
+        'votes'
+    ];
 }
--- routes/web.php
+++ routes/web.php
@@ -63,7 +63,68 @@ Route::get('/posts/{id}', function($id){
 
     // $post = Post::all()->first();
 
-    $post = Post::find([$id, 1])->take(2);
+    // $post = Post::find([$id, 1])->take(2);
 
-    return $post;
+    // SAVE
+    // $post = new Post;
+    // $post->title = 'Netherlands is awesome';
+    // $post->body = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt expedita quas odit esse beatae facilis ad, quod voluptatum atque, cum aliquam temporibus animi ut at mollitia veritatis delectus quasi tenetur.';
+    // $post->votes = 5;
+    // $post->is_admin = 1;
+    // $post->save();
+
+    // UPDATE
+    // $posts = Post::find(1);
+    // $posts->title = 'Awesome Titans';
+    // $posts->save();
+
+    // MASS UPDATE
+    // $posts = Post::where('is_admin',1)->update([
+    //     'votes' => 100
+    // ]);
+
+    // MASS ASSIGNMENT
+    // Add these to fillables in Model
+    // Post::create([
+    //     'title' => 'Mass Assignment',
+    //     'body' => 'Content fake',
+    //     'votes' => 99,
+    //     'is_admin' => 1
+    // ]);
+
+    // DELETE
+    // if you want to find one
+    // $post = Post::find(4);
+    // $post->delete();
+
+    // if you know which one
+    // Post::destroy(3);
+
+    // delete multiple
+    // Post::destroy([1,2,3]);
+
+    // return $post;
+
+    // Softdelete
+    // add softdelete in migration
+    // use the method in the model
+
+    // $post = Post::find(6);
+    // $post->delete();
+
+    // wont show deleted_at
+    // $post = Post::all();
+
+    // also show deleted_at
+    // $post = Post::withTrashed()->get();
+
+    // only softdeleted
+    // $post = Post::onlyTrashed()->get();
+
+    // delete trashed posts
+    $post = Post::onlyTrashed()
+        ->where('id', 6)
+        ->forceDelete();
+    
+    // return $post;
 });
